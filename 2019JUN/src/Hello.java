95efaf7f7bb6feef2513c990601af62c31d18347
davidseungjin <ryonlee@gmail.com>
2019-06-17 14:26:10 -0700

    java

---
+++ 2019JUN/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Team");
+        System.out.println("Hello David. This is IntelliJ application and it is your first JAVA program I believe.");
+
+    }
+}
