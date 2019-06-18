00e34db464ada52643753e321062e6da3a611a51
davidseungjin <ryonlee@gmail.com>
2019-06-18 10:19:12 -0700

    java

--- 2019JUN/src/Hello.java
+++ 2019JUN/src/Hello.java
@@ -6,5 +6,22 @@ public class Hello {
         System.out.println("Hello, Team");
         System.out.println("Hello David. This is IntelliJ application and it is your first JAVA program I believe.");
 
+        int myfirstNumber = 5*2;
+        int mysecondNumber = 10 + 5;
+        int myTotal = myfirstNumber + mysecondNumber;
+
+        int myRecursion = 2;
+        myRecursion = myRecursion + myTotal;
+
+        System.out.println(myfirstNumber);
+        System.out.println(mysecondNumber);
+        System.out.println(myTotal);
+
+        System.out.println(myRecursion);
+
+        myRecursion = myRecursion + myTotal;
+        System.out.println(myRecursion);
+
+
     }
 }
---
+++ Second/src/com/david/Main.java
@@ -0,0 +1,33 @@
+package com.david;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myValue = 1000;
+
+//        int has a width of 32
+        int myMinvalue = -2_147_483_648;
+        int myMaxvalue = 2_147_483_647;
+
+//        int error = myMaxvalue + 1; <=== this is error becuase it exceeds integer limit
+
+//        byte has a width of 8
+        byte myByteMax = 127;
+        byte myByteMin = -128;
+        byte asdf = (byte) (myByteMax/myValue);
+
+//        short has a width of 16
+        short myShort = 32767;
+
+//        Long has a width of 64 2 to the power of 64.
+        long myLong = 100L;
+
+        double asasas = 1.25;
+
+        System.out.println("long value myLong is " + myLong);
+        System.out.println("asdf value is " + asdf);
+        System.out.println("double value is " + asasas);
+
+    }
+}
