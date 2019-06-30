b5cf78a8fff15e9d0caf1b8ef8f1658526922547
David Seungjin Lee <41690461+davidseungjin@users.noreply.github.com>
2019-06-29 20:49:49 -0700

    Update DrumKit.java

--- HFJava/DrumKit.java
+++ HFJava/DrumKit.java
@@ -6,16 +6,3 @@ class DrumKit{
 		System.out.println("ding ding da-ding");
 	}
 }
-
-class DrumKitTestDrive{
-	public static void main(String[] args){
-		DrumKit d = new DrumKit();
-		d.playSnare();
-		d.snare = false;
-		d.playTopHat();
-
-		if(d.snare == true){
-			d.playSnare();
-		}
-	}
-}
