d879c552ce48b4e61277d6319705a85e45f6931c
davidseungjin <ryonlee@gmail.com>
2019-07-24 18:19:05 -0700

    java

---
+++ JUL18/src/com/david/Main.java
@@ -0,0 +1,11 @@
+package com.david;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int number = 1; number < 7; number += 2){
+            System.out.println("number= " + number);
+        }
+    }
+}
