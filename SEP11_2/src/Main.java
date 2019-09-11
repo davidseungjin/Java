8a0f44b4a6b244c0569b4160bb1517a65ee218ba
davidseungjin <ryonlee@gmail.com>
2019-09-11 14:54:07 -0700

    java

--- SEP11_2/src/Main.java
+++ SEP11_2/src/Main.java
@@ -4,18 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();     //handle next line character (enter key)
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2018 - yearOfBirth;
-        if(age>=0 && age<=150){
-            System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        System.out.println("Enter your year of birth: ");
+
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if(hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();     //handle next line character (enter key)
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2018 - yearOfBirth;
+            if(age>=0 && age<=150){
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            }else{
+                System.out.println("Invalid input");
+            }
         }else{
-            System.out.println("Invalid input");
+            System.out.println("Invalid input: You input String for Int");
         }
         scanner.close();
     }
